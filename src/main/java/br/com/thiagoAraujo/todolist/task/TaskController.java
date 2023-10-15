a99ff567b89e18605f8889f47a6196da5be58c61
Thiago Araujo <thiagoaraujo43034@gmail.com>
2023-10-15 18:18:50 -0300

    <refactor>[Exclusão de importações não utilizadas]

--- src/main/java/br/com/thiagoAraujo/todolist/task/TaskController.java
+++ src/main/java/br/com/thiagoAraujo/todolist/task/TaskController.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-//import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;

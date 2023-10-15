2355f380194fd82d14c11c77aee2035d9acae8e7
Thiago Araujo <thiagoaraujo43034@gmail.com>
2023-10-15 18:50:36 -0300

    <feat>[Altualização parcial]: Implementação da possíbilidade de altualização parcial das propriedades das tarefas

--- src/main/java/br/com/thiagoAraujo/todolist/task/TaskController.java
+++ src/main/java/br/com/thiagoAraujo/todolist/task/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.thiagoAraujo.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 
@@ -51,8 +52,11 @@ public class TaskController {
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
         var IdUser = request.getAttribute("IdUser");
-        taskModel.setIdUser((UUID)IdUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+       
+        return this.taskRepository.save(task);
     }
 }

66c3332e0e7071bd1f7ecf5670081503dd7af1ce
Thiago Araujo <thiagoaraujo43034@gmail.com>
2023-10-16 17:57:12 -0300

    <feat>[Tratamento de erros]: Implementação de menssgens para tratamento de erros no cadastro da tarefa

--- src/main/java/br/com/thiagoAraujo/todolist/task/TaskController.java
+++ src/main/java/br/com/thiagoAraujo/todolist/task/TaskController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import br.com.thiagoAraujo.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,7 +21,6 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/tasks/")
 public class TaskController {
-
 
     @Autowired
     private ITaskRepository taskRepository;
